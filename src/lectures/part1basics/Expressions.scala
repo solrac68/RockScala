3bd01fc2f816ea25ba587bce8b2962a6e9328855
carlosubuntu <solrac.hincapie@gmail.com>
2019-12-08 07:36:23 -0500

    SE termina la sección de expressions

--- src/lectures/part1basics/Expressions.scala
+++ src/lectures/part1basics/Expressions.scala
@@ -9,9 +9,45 @@ object Expressions extends App {
 
   println((!(1 == x)))
 
-  println("Hola, Scala")
-  println("Hola, Scala")
-  println("Hola, Scala")
+  var aVariable = 2
+  aVariable += 3 // also works with -= *= /=
+
+  println(aVariable)
+
+  // Instructions (DO) vs Expressions (VALUE)
+
+  // IF expressions
+  val aCondition = true
+  val aConditionValue = if(aCondition) 5 else 4 // IF EXPRESSIONS
+  println(aConditionValue)
+
+  var i = 0
+  while(i < 10){
+    println(i)
+    i += 1
+  }
+  // never write this again
+
+  // Everything in Scala is an Expressions
+
+  val aWeirdValue = (aVariable = 3) // Unit === void
+
+  println(aWeirdValue)
+
+  // side effects: println(), whiles, reassigning
+
+  // Code Block
+
+  val aCodeBlock = {
+    val y = 2
+    val z = y + 1
+
+    if(z > 2) "hello" else "good bye"
+  }
+
+
+
+
 
 
 }
