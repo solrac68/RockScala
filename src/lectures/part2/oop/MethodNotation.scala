f6bddb139635b5bde04af1f40997abed52b61d9e
Carlos Augusto Hincapié R <solrac.hincapie@gmail.com>
2020-02-06 16:38:47 -0500

    Se agrega la clase MethodNotation Excercise

--- src/lectures/part2/oop/MethodNotation.scala
+++ src/lectures/part2/oop/MethodNotation.scala
@@ -1,12 +1,16 @@
 package lectures.part2.oop
 
 object MethodNotation extends App{
-  class Person(val name: String, favoriteMovie: String){
+  class Person(val name: String, favoriteMovie: String, age:Int = 20){
     def likes(movie: String):Boolean = movie == favoriteMovie
     def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
     def unary_! : String = s"$name, what the heck?!"
     def isAlive: Boolean = true
     def apply(): String = s"Hi my name is $name and I like $favoriteMovie"
+    def apply(twist:Int): String = s"$name watched $twist $favoriteMovie"
+    def +(nickName: String): Person = new Person(s"${this.name} (${nickName})",favoriteMovie)
+    def unary_+ : Person = new Person(this.name, this.favoriteMovie, age + 1)
+    def print(): Unit = println(s"Name is $name, favoriteMovie is $favoriteMovie, age: $age")
   }
 
   val mary = new Person("Mary","Inception")
@@ -38,6 +42,14 @@ object MethodNotation extends App{
   println(mary.apply())
   println(mary()) // same effect that mary.apply()
 
+  //println(mary.+("La lectora"))
+
+  println((mary + "La aviona")()) // Hi my name is Mary (La aviona) and I like Inception
+
+  mary.print()
+  (+(+mary)).print()
+
+  println(mary(3))
 
 
 
