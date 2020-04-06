933ac295cb5b79b9b53cc61f359a400a5d4b4095
Carlos Augusto Hincapié R <solrac.hincapie@gmail.com>
2020-04-06 17:13:00 -0500

    Paquetes

---
+++ src/lectures/part2/oop/PackagingAndImports.scala
@@ -0,0 +1,21 @@
+package lectures.part2.oop
+
+import playground.Cinderella
+
+object PackagingAndImports extends App{
+
+  // Import the package
+  val princess = new Cinderella // import playground.Cinderella
+  val princess2 = new playground.Cinderella // fully qualified name
+
+  // packages are in hierarchy
+  // matching folder structure
+
+  // package object
+  sayHello
+  println(SPEED_OF_LIGHT)
+
+
+
+
+}
---
+++ src/lectures/part2/oop/package.scala
@@ -0,0 +1,6 @@
+package lectures.part2
+
+package object oop {
+  def sayHello:Unit = println("Hello, Scala")
+  val SPEED_OF_LIGHT = 299792458
+}
