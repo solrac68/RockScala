d469a92fbf01721be31ac20bf475655a1a024221
Carlos Augusto Hincapié R <solrac.hincapie@gmail.com>
2020-04-08 12:38:24 -0500

    part3fp

--- src/lectures/part2/oop/PackagingAndImports.scala
+++ src/lectures/part2/oop/PackagingAndImports.scala
@@ -1,11 +1,18 @@
 package lectures.part2.oop
 
-import playground.Cinderella
+import java.sql
+import java.sql.SQLData
+
+import playground.{PrinceCharming, Cinderella => Princess}
+//import playground._
+
+import java.util.Date
+import java.sql.{Date => SqlDate}
 
 object PackagingAndImports extends App{
 
   // Import the package
-  val princess = new Cinderella // import playground.Cinderella
+  val princess = new Princess // import playground.{Cinderella => Princess}
   val princess2 = new playground.Cinderella // fully qualified name
 
   // packages are in hierarchy
@@ -14,6 +21,22 @@ object PackagingAndImports extends App{
   // package object
   sayHello
   println(SPEED_OF_LIGHT)
+
+  // Imports
+  // import playground.{Cinderella, PrinceCharming} or
+  // import playground._
+  val prince = new PrinceCharming
+
+  val date = new Date() // take from import java.util.Date
+  // 1. use FQ names
+  val sqlDate = new java.sql.Date(2019)
+  val sqlDate2 = new SqlDate(2020) // take import java.sql.{Date => SqlDate}
+
+  // 2. use aliasing
+  // defaults imports
+  // java.lang - String, Object, Exception
+  // scala - Int, Nothing, Function
+  // scala.Predef - println, ???
 
 
 
